ee93c3ba353b76a89358a7bb3cde74e108bc3800
EvanGudmestad <eagudmestad@ranken.edu>
2026-08-28 13:08:38 -0500

    Replaced string types with literal types

--- src/types/jobApplication.ts
+++ src/types/jobApplication.ts
@@ -3,4 +3,5 @@ export interface JobApplication {
   jobId: string;
   providerId: string;
   appliedAt: string;
+  providerResponse?: "accepted" | "declined" //Like an uber driver responding to a ride
 }
