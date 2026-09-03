ab8e0a879828beebe5a920e48617b6da72c167ec
EvanGudmestad <eagudmestad@ranken.edu>
2026-09-03 14:59:02 -0500

    created express server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/health`);
+});
